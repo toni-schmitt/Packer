58088036a5d840a9f5abd7a1de12e1c9a745a853
Toni Schmitt <tonischmitt@online.de>
2020-11-22 14:30:12 +0100

    added Values Class for storing Values like Paths, Directorys and names

---
+++ Packer/Values.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer
+{
+    public static class Values
+    {
+        public static string sourceFilePath;
+        public static string sourceFileDirectory;
+        public static string sourceFileName;
+
+        public static string destFilePath;
+        public static string destFileDirectory;
+        public static string destFileName;
+
+        public static char marker = '{';
+
+        public static string header = "ttpack{";
+    }
+}
